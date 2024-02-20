b58bb2888a3ab3f11922e1839e3716a03f990d62
jmao5 <jsh5890@kakao.com>
2024-02-20 23:56:38 +0900

    init cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,18 @@
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import tailwind from "@astrojs/tailwind";
+import solidJs from "@astrojs/solid-js";
+import { SITE_METADATA } from "./src/consts.ts";
+import metaTags from "astro-meta-tags";
 
+import robotsTxt from "astro-robots-txt";
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
+  site: SITE_METADATA.siteUrl,
+  integrations: [mdx(), sitemap(), tailwind(), solidJs(), metaTags(), robotsTxt()],
   output: "server",
   adapter: cloudflare()
 });
